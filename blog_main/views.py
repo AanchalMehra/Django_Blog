177538c9ebc2554b2ac23a66fb119031cf19c6f9
AanchalMehra <aanchalmehra15@gmail.com>
2026-07-23 19:10:04 +0530

    Implementation of posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from blogs.models import Category, Blog
+
 def home(request):
-    return render(request,'home.html')
+  
+    featured_posts=Blog.objects.filter(isFeatured=True, status='Published').order_by('-updated_at')
+    Not_featured_posts=Blog.objects.filter(isFeatured=False, status='Published').order_by('-updated_at')
+    context={
+        
+        'featured_posts':featured_posts,
+        'not_featured_posts':Not_featured_posts
+    }
+    return render(request,'home.html',context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Blog, Category
 
 # Create your views here.
+def posts_by_category(request, pk):
+    posts=Blog.objects.filter(category=pk, status='Published').order_by('-created_at')
+    category=get_object_or_404(Category, pk=pk)
+    '''try:
+        category=Category.objects.get(pk=pk)
+    except:
+        return redirect('home')'''
+    context={'posts':posts,'category':category}
+    return render(request, 'post_by_category.html', context)
